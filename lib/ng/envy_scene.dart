c747b3234c98376e7b3fe054d204efa7b1ca1b00
Rob Bishop <robbishop65@gmail.com>
2017-06-26 06:15:08 -0400

    wheel panel

--- lib/ng/envy_scene.dart
+++ lib/ng/envy_scene.dart
@@ -1,9 +1,10 @@
+import 'dart:html';
 import 'package:angular2/angular2.dart';
 import 'package:envy/envy.dart';
 
+//encapsulation: ViewEncapsulation.Native,
 @Component(
     selector: "envy-scene",
-    encapsulation: ViewEncapsulation.Native,
     templateUrl: 'envy_scene.html')
 class EnvyScene implements AfterViewInit {
   //TODO input spec
@@ -19,6 +20,12 @@ class EnvyScene implements AfterViewInit {
     //print("Envy div ATTACHED");
     sceneGraph.htmlHost = envyRootEl.nativeElement;
     //sceneGraph.updateGraph();
+  }
+
+  /// Returns the first canvas element found under the root (or null).
+  CanvasElement get canvas {
+    Element root = envyRootEl.nativeElement;
+    return root?.querySelector("canvas") as CanvasElement;
   }
 
 }
