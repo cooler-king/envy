98794fb6b33951f6d824c16799a7efcd7ef2d79b
Rob Bishop <robbishop65@gmail.com>
2018-04-15 21:43:32 -0400

    dev.48

--- example/main.dart
+++ example/main.dart
@@ -1,9 +1,10 @@
 import 'package:angular/angular.dart';
 import 'gallery.dart';
 
-// ignore: URI_HAS_NOT_BEEN_GENERATED
+// ignore_for_file: URI_HAS_NOT_BEEN_GENERATED
+import 'gallery.template.dart' as app;
 import 'main.template.dart' as ng;
 
 void main() {
-  bootstrapStatic<dynamic>(Gallery, <dynamic>[], ng.initReflector as Function);
+  runApp<Gallery>(app.GalleryNgFactory as ComponentFactory<Gallery>);
 }
